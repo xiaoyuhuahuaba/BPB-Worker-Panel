cff76e438646280972bf46011a40729588124ecc
xiaoyuhuahuaba <161581439+xiaoyuhuahuaba@users.noreply.github.com>
2024-07-18 14:59:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'e1f1ad37-4ff2-432c-84cf-d4a2491376ef';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
